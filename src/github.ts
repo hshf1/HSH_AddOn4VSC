e549b245549f2a7107e30edd22d7032baf2c1575
faBi292 <fabian.norrmann@gmail.com>
2023-04-17 15:52:27 +0200

    Encoding in Github.ts angepasst

--- src/github.ts
+++ src/github.ts
@@ -1,36 +1,36 @@
-/** Diese Modul hat die Aufgabe sich mit GitHub zu verbinden und die "n걎zlichen Links" zu kopieren und richtig einzuspeichern */
+/** Diese Modul hat die Aufgabe sich mit GitHub zu verbinden und die "n체tzlichen Links" zu kopieren und richtig einzuspeichern */
 
 import { get } from 'request' /** Importiert die get Funktion aus dem Node.js Modul, damit lassen sich URL's aufrufen */
 
-let githublinks: { link: string, name: string }[] = [] /** Definiert ein Objekt Array mit den Eigenschaten "link und "name" f걊 dei einzelnen Links  */
+let githublinks: { link: string, name: string }[] = [] /** Definiert ein Objekt Array mit den Eigenschaten "link und "name" f체r dei einzelnen Links  */
 let github_status: boolean | undefined = undefined  //* Definiert eine Variable die den Status der Verbindung zum GitHub Server wieder gibt */
 
 get('https://raw.githubusercontent.com/hshf1/HSH_AddOn4VSC/master/script/links.txt', (error, response, body) => {
-/** Versucht die URL aufzurufen, falls ein Fehler entsteht wird der in Error gespeichert, response enth꼕t das HTTP response Objekt und der Body enth꼕t den body als String */
-    if (error) { /** Wenn ein Fehler aufgetreten ist, wird der ausgegeben und der Status wird auf flase gesetzt */
+/** Versucht die URL aufzurufen, falls ein Fehler entsteht wird der in Error gespeichert, response enth채lt das HTTP response Objekt und der Body enth채lt den body als String */
+    if (error) { /** Wenn ein Fehler aufgetreten ist, wird der ausgegeben und der Status wird auf false gesetzt */
         console.error(error)
         github_status = false
     } else {
-        const elements = body.replace(/[\n]/g, '').split('<-') /** Entfernt die Zeilenumbr갷he und trennt die Kette an den Stellen "<-" in ein Array auf  */
+        const elements = body.replace(/[\n]/g, '').split('<-') /** Entfernt die Zeilenumbr체che und trennt die Kette an den Stellen "<-" in ein Array auf  */
         let currentLink: { link: string, name: string } = { link: "", name: "" } /** Erzeugt Objekt mit Eigenschaften link und name und stellt sicher das beide Argumente leer sind */
         for (const element of elements) {   /** For Schleife durch die aufgetrenten Elemente */
             const [property, value] = element.split('->') /** Erzeugt neues Objekt mit den Eigenschaften property und value, danach trennt es das Element nochmals auf und speichert es in das neue Objekt */
-            if (property.startsWith('name')) { /** Wenn property name enth꼕t wird der name getrimmed (whitespace entfernt) und eingespeichert */
+            if (property.startsWith('name')) { /** Wenn property name enth채lt wird der name getrimmed (whitespace entfernt) und eingespeichert */
                 currentLink.name = value.trim()
-            } else if (property.startsWith('link')) {/** Wenn property link enth꼕t wird der link getrimmed (whitespace entfernt) und eingespeichert */
+            } else if (property.startsWith('link')) {/** Wenn property link enth채lt wird der link getrimmed (whitespace entfernt) und eingespeichert */
                 currentLink.link = value.trim()
-                githublinks.push(currentLink) /** Das Objekt wird nun an GithubLinks 갶ergeben  */
-                currentLink = { link: "", name: "" } /** Currentlink wird wieder zur갷kgesetzt */
+                githublinks.push(currentLink) /** Das Objekt wird nun an GithubLinks 체bergeben  */
+                currentLink = { link: "", name: "" } /** Currentlink wird wieder zur체ckgesetzt */
             }
         }
         github_status = true    /** Wenn alle Links eingelesen sind wird der Status auf true gesetzt */
     }
 })
 
-export function getGithubStatus() { /** Globale Funktion die den Github Status zur갷kgeben kann*/ 
+export function getGithubStatus() { /** Globale Funktion die den Github Status zur체ckgeben kann*/ 
     return github_status
 }
 
-export function getGithubLinks() {  /** Globale Funktion die die GitHub Links zur갷kgeben kann*/ 
+export function getGithubLinks() {  /** Globale Funktion die die GitHub Links zur체ckgeben kann*/ 
     return githublinks
 }
