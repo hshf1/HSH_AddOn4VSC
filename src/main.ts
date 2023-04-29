e3457550796946ab35dac726d4b031af2bee3337
cakohsh <can.kocak@stud.hs-hannover.de>
2023-04-29 11:30:46 +0200

    uncaughtexceptions added

--- src/main.ts
+++ src/main.ts
@@ -48,6 +48,10 @@ export async function activate(context: ExtensionContext) {
 			onEventProgLanguage()
 		}
 	})
+	process.on('uncaughtException', (error) => {
+		console.log("UNCAUGHT ERROR: " + error.name + error.message + error)
+	  });
+	  
 	getCommands().forEach(command => { /** For Schleife durch alle "command" Objekte in "registercommands.ts". name: name des commands, callback: Funktion die ausgeführt wird */
 		/** Durch "context.subscriptions.push" wird das Objekt nach deaktivieren der Erweiterung ordnungsgemäss aufgeräumt */
 		context.subscriptions.push(commands.registerCommand(command.name, command.callback))
