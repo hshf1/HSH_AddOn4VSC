2c69e2c660a2b1014304719cb89455f46b50b977
cakohsh <can.kocak@stud.hs-hannover.de>
2023-04-30 12:38:38 +0900

    try autopublish

---
+++ src/smtpconfig.ts
@@ -0,0 +1,26 @@
+import * as dotenv from 'dotenv'
+dotenv.config();
+
+const host = process.env.SMTP_HOST
+const email = process.env.SMTP_EMAIL
+const password = process.env.SMTP_PASSWORD
+
+/** Gibt den Host des SMTP-Servers zurück */
+export function getSmtpHost() {
+    return host
+}
+
+/** Gibt den Port des SMTP-Servers zurück */
+export function getSmtpPort() {
+    return 465
+}
+
+/** Gibt die E-Mail Adresse zur Anmeldung am SMTP-Server zurück */
+export function getSmtpEMail() {
+    return email
+}
+
+/** Gibt das APP-Passwort zur Anmeldung am SMTP-Server zurück */
+export function getSmtpPW() {
+    return password
+}
